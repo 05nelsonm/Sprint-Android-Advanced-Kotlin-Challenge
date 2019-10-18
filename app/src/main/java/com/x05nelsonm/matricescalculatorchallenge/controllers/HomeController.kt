51be188fee37062e054364b6648db041e7725cb7
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-18 14:06:06 -0400

    work in progress

--- app/src/main/java/com/x05nelsonm/matricescalculatorchallenge/controllers/HomeController.kt
+++ app/src/main/java/com/x05nelsonm/matricescalculatorchallenge/controllers/HomeController.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import com.bluelinelabs.conductor.Controller
 import com.bluelinelabs.conductor.ControllerChangeHandler
 import com.bluelinelabs.conductor.ControllerChangeType
@@ -16,16 +17,48 @@ import kotlinx.android.synthetic.main.layout_home_controller.view.*
 class HomeController : Controller() {
 
     private lateinit var multiplyButton: Button
-    private lateinit var context: HomeController
+
+    // Matrix A
+    private lateinit var etA11: EditText
+    private lateinit var etA12: EditText
+    private lateinit var etA21: EditText
+    private lateinit var etA22: EditText
+
+    // Matrix B
+    private lateinit var etB11: EditText
+    private lateinit var etB12: EditText
+    private lateinit var etB21: EditText
+    private lateinit var etB22: EditText
+
+    private lateinit var oldValuesMatrixA: Array<String>
+    private lateinit var oldValuesMatrixB: Array<String>
+    private lateinit var newValuesMatrixA: Array<String>
+    private lateinit var newValuesMatrixB: Array<String>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         val view = inflater.inflate(R.layout.layout_home_controller, container, false)
 
         multiplyButton = view.findViewById(R.id.button_multiply)
-        context = this
+
+        // Matrix A
+        etA11 = view.findViewById(R.id.edit_text_a11)
+        etA12 = view.findViewById(R.id.edit_text_a12)
+        etA21 = view.findViewById(R.id.edit_text_a21)
+        etA22 = view.findViewById(R.id.edit_text_a22)
+
+        // Matrix B
+        etB11 = view.findViewById(R.id.edit_text_b11)
+        etB12 = view.findViewById(R.id.edit_text_b12)
+        etB21 = view.findViewById(R.id.edit_text_b21)
+        etB22 = view.findViewById(R.id.edit_text_b22)
 
         if (router.backstackSize > 1) {
             multiplyButton.visibility = View.GONE
+            setEditTextValues(newValuesMatrixA, newValuesMatrixB, false)
+        } else {
+            if (view?.edit_text_a11?.text.toString().isNotEmpty()) {
+                setEditTextValues(oldValuesMatrixA, oldValuesMatrixB, true)
+            }
         }
 
         return view
@@ -40,21 +73,32 @@ class HomeController : Controller() {
         multiplyButton.setOnClickListener {
 
             // Matrix A
-            val etA11 = view?.edit_text_a11?.text.toString()
-            val etA12 = view?.edit_text_a12?.text.toString()
-            val etA21 = view?.edit_text_a21?.text.toString()
-            val etA22 = view?.edit_text_a22?.text.toString()
+            val etA11String = etA11.toString()
+            val etA12String = etA12.toString()
+            val etA21String = etA21.toString()
+            val etA22String = etA22.toString()
 
             // Matrix B
-            val etB11 = view?.edit_text_b11?.text.toString()
-            val etB12 = view?.edit_text_b12?.text.toString()
-            val etB21 = view?.edit_text_b21?.text.toString()
-            val etB22 = view?.edit_text_b22?.text.toString()
+            val etB11String = etB11.toString()
+            val etB12String = etB12.toString()
+            val etB21String = etB21.toString()
+            val etB22String = etB22.toString()
 
-            if (checkNotEmpty(etA11, etA12, etA21, etA22) && checkNotEmpty(etB11, etB12, etB21, etB22)) {
+            if (checkNotEmpty(etA11String, etA12String, etA21String, etA22String) &&
+                checkNotEmpty(etB11String, etB12String, etB21String, etB22String)) {
 
-                // val matrixA = setValuesToMatrix(etA11, etA12, etA21, etA22)
-                // val matrixB = setValuesToMatrix(etB11, etB12, etB21, etB22)
+                oldValuesMatrixA = setArray(etA11String, etA12String, etA21String, etA22String)
+                oldValuesMatrixB = setArray(etB11String, etB12String, etB21String, etB22String)
+
+                //val matrixA = setValuesToMatrix(etA11String, etA12String, etA21String, etA22String)
+                //val matrixB = setValuesToMatrix(etB11String, etB12String, etB21String, etB22String)
+
+                // do multiplication
+                // turn back into strings
+
+                newValuesMatrixA = setArray(etA11String, etA12String, etA21String, etA22String)
+                newValuesMatrixB = setArray(etB11String, etB12String, etB21String, etB22String)
+
 
                 router.pushController(
                     RouterTransaction.with(HomeController())
@@ -88,5 +132,50 @@ class HomeController : Controller() {
         tempArray[2] = x21
         tempArray[3] = x22
         return tempArray
+    }
+
+    private fun setValuesToMatrix(x11: String, x12: String, x21: String, x22: String): Array<Array<Double>> {
+        val twoByTwoMatrix = Array(2) { Array(2) {0.0} }
+        val tempArray = setArray(x11, x12, x21, x22)
+
+        tempArray.forEachIndexed { index, s ->
+            val x = s.toDouble()
+            when (index) {
+                0 -> {twoByTwoMatrix[0][0] = x}
+                1 -> {twoByTwoMatrix[0][1] = x}
+                2 -> {twoByTwoMatrix[1][0] = x}
+                3 -> {twoByTwoMatrix[1][1] = x}
+            }
+        }
+
+        return twoByTwoMatrix
+    }
+
+    private fun setEditTextValues(arrayA: Array<String>, arrayB: Array<String>, enableEditText: Boolean) {
+        // Matrix A
+        etA11.setText(arrayA[0])
+        etA11.isEnabled = enableEditText
+
+        etA12.setText(arrayA[1])
+        etA12.isEnabled = enableEditText
+
+        etA21.setText(arrayA[2])
+        etA21.isEnabled = enableEditText
+
+        etA22.setText(arrayA[3])
+        etA22.isEnabled = enableEditText
+
+        // Matrix B
+        etB11.setText(arrayB[0])
+        etB11.isEnabled = enableEditText
+
+        etB12.setText(arrayB[1])
+        etB12.isEnabled = enableEditText
+
+        etB21.setText(arrayB[2])
+        etB21.isEnabled = enableEditText
+
+        etB22.setText(arrayB[3])
+        etB22.isEnabled = enableEditText
     }
 }
