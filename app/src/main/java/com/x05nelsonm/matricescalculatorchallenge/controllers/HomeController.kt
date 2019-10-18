87c31fb865685e0772daa0cbd1f9ee4b09be9a13
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-18 12:14:50 -0400

    built layout for home countroller

--- app/src/main/java/com/x05nelsonm/matricescalculatorchallenge/controllers/HomeController.kt
+++ app/src/main/java/com/x05nelsonm/matricescalculatorchallenge/controllers/HomeController.kt
@@ -4,9 +4,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
+import com.x05nelsonm.matricescalculatorchallenge.R
+import kotlinx.android.synthetic.main.layout_home_controller.view.*
 
 class HomeController : Controller() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = inflater.inflate(R.layout.layout_home_controller, container, false)
+
+        val etA11 = view.edit_text_a11.text.toString()
+        val etA12 = view.edit_text_a12.text.toString()
+        val etA21 = view.edit_text_a21.text.toString()
+        val etA22 = view.edit_text_a22.text.toString()
+        val etB11 = view.edit_text_b11.text.toString()
+        val etB12 = view.edit_text_b12.text.toString()
+        val etB21 = view.edit_text_b21.text.toString()
+        val etB22 = view.edit_text_b22.text.toString()
+
+        return view
     }
 }
